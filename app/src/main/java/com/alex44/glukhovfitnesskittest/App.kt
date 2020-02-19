4e47f9352425b9e217f458715fafb5f3f3addbc3
Alexandr44 <alexandrgluhov@mail.ru>
2020-02-19 23:10:00 +0500

    Класс App

---
+++ app/src/main/java/com/alex44/glukhovfitnesskittest/App.kt
@@ -0,0 +1,26 @@
+package com.alex44.glukhovfitnesskittest
+
+import android.app.Application
+import com.alex44.glukhovfitnesskittest.di.AppComponent
+import com.alex44.glukhovfitnesskittest.di.DaggerAppComponent
+import com.alex44.glukhovfitnesskittest.di.modules.AppModule
+import timber.log.Timber
+
+class App : Application() {
+
+    companion object{
+        lateinit var instance : App
+    }
+
+    lateinit var appComponent : AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Timber.plant(Timber.DebugTree())
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
+}
