740dff1adf1483f45b0377a3a93055a34935bdee
Alexandr44 <alexandrgluhov@mail.ru>
2020-02-21 06:01:24 +0500

    Добавил запись и чтения данных из локальной базы

--- app/src/main/java/com/alex44/glukhovfitnesskittest/di/modules/RepoModule.kt
+++ app/src/main/java/com/alex44/glukhovfitnesskittest/di/modules/RepoModule.kt
@@ -2,15 +2,17 @@ package com.alex44.glukhovfitnesskittest.di.modules
 
 import com.alex44.glukhovfitnesskittest.common.interfaces.INetworkStatus
 import com.alex44.glukhovfitnesskittest.model.api.IDataSource
+import com.alex44.glukhovfitnesskittest.model.cache.IDataCache
 import com.alex44.glukhovfitnesskittest.model.repo.DataRepo
 import com.alex44.glukhovfitnesskittest.model.repo.IDataRepo
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
-@Module(includes = [ApiModule::class, NetworkModule::class])
+@Module(includes = [ApiModule::class, NetworkModule::class, CacheModule::class])
 class RepoModule {
 
     @Provides
-    fun repo(dataSource : IDataSource, networkStatus : INetworkStatus) : IDataRepo = DataRepo(dataSource, networkStatus)
+    fun repo(dataSource : IDataSource, networkStatus : INetworkStatus, @Named("Room") dataCache : IDataCache) : IDataRepo = DataRepo(dataSource, networkStatus, dataCache)
 
 }
--- app/src/main/java/com/alex44/glukhovfitnesskittest/model/repo/DataRepo.kt
+++ app/src/main/java/com/alex44/glukhovfitnesskittest/model/repo/DataRepo.kt
@@ -2,12 +2,17 @@ package com.alex44.glukhovfitnesskittest.model.repo
 
 import com.alex44.glukhovfitnesskittest.common.interfaces.INetworkStatus
 import com.alex44.glukhovfitnesskittest.model.api.IDataSource
+import com.alex44.glukhovfitnesskittest.model.cache.IDataCache
 import com.alex44.glukhovfitnesskittest.model.dto.LessonDTO
 import io.reactivex.Maybe
 import io.reactivex.MaybeEmitter
 import io.reactivex.schedulers.Schedulers
 
-class DataRepo(private var source: IDataSource, private val networkStatus: INetworkStatus) : IDataRepo {
+class DataRepo(
+    private var source: IDataSource,
+    private val networkStatus: INetworkStatus,
+    private val dataCache: IDataCache
+) : IDataRepo {
 
     override fun getData(): Maybe<List<LessonDTO>> {
         if (networkStatus.isOnline()) {
@@ -15,13 +20,21 @@ class DataRepo(private var source: IDataSource, private val networkStatus: INetw
                 .subscribeOn(Schedulers.io())
                 .map { list ->
                     return@map list.filter { lessonDTO ->
-                        return@filter lessonDTO != null && !lessonDTO.name.isNullOrBlank()
+                        return@filter lessonDTO != null && !lessonDTO.appointmentId.isNullOrBlank()
+                    }.apply {
+                        dataCache.putData(this)
                     }
                 }
         }
         else {
             return Maybe.create { emitter: MaybeEmitter<List<LessonDTO>> ->
-                emitter.onError(RuntimeException("No internet"))
+                val list = dataCache.getData()
+                if (list.isNullOrEmpty()) {
+                    emitter.onError(java.lang.RuntimeException("No internet connection. No data in local storage"))
+                }
+                else {
+                    emitter.onSuccess(list)
+                }
             }
                 .subscribeOn(Schedulers.io())
         }
